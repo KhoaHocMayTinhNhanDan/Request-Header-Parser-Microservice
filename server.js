8bcd7f610f3cb97985d45cea22785f1d87a9b103
An Dong <hoangdonghvcs@gmail.com>
2016-11-16 13:47:26 +0000

    modified server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World! hihihi')
-})
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.get('/', function(req, res) {
+    if (req.url === '/favicon.ico') return;
+    res.json({
+        ipaddress: req.headers['x-forwarded-for'] ||
+                   req.connection.remoteAddress,
+        language: req.headers["accept-language"].split(",")[0],
+        software: req.headers["user-agent"].match(/\((.*?)\)/)[1]
+    });
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
